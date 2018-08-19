f90bd6ed0fb2f78ee17a3dcfaa3af475a00ccbbd
Manu <manuhazen@outlook.com>
2018-08-19 12:23:25 -0400

    Single movie update the component when change

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -3,8 +3,7 @@ import { connect } from 'react-redux';
 
 import MovieHeader from '../components/MovieHeader';
 import MoviesList from '../components/MoviesList';
-import ActorList from '../components/ActorsList';
-import ReviewList from '../components/ReviewsList';
+import ReviewsList from '../components/ReviewsList';
 
 import {
   getMovie,
@@ -15,6 +14,7 @@ import {
   cleanMovieState
 } from '../actions/movieActions';
 
+
 export class MoviePage extends Component {
 
   componentWillMount() {
@@ -24,6 +24,19 @@ export class MoviePage extends Component {
     this.props.getMoviesReviews(id);
     this.props.getRelatedMovies(id);
     this.props.getMovieActors(id);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.match.params.id !== this.props.match.params.id) {
+      this.props.cleanMovieState();
+      
+      const {id} = this.props.match.params;
+      this.props.getMovie(id);
+      this.props.getMovieVideos(id);
+      this.props.getMoviesReviews(id);
+      this.props.getRelatedMovies(id);
+      this.props.getMovieActors(id);
+    }
   }
 
   componentWillUnmount() {
@@ -38,8 +51,20 @@ export class MoviePage extends Component {
             actors={this.props.actors} productionCompanies={this.props.productionCompanies}/>
         </div>
         <div className="col-md-12">
-          <div>ReviewList</div>
-          <div>MovieCardList</div>
+          {
+            this.props.reviews.length ?
+              <div className="col-md-12">
+                <h1 className="text-center h3" style={{ 'padding': '.6em 0px .3em 10px' }}>Reviews</h1>
+                <ReviewsList reviews={this.props.reviews} />
+              </div> : null
+          }
+          {
+            this.props.relatedMovies.length ?
+            <div className="col-md-12">
+              <h1 className="text-center h3" style={{ 'padding': '.6em 0px .3em 10px' }}>Related Movies</h1>
+              <MoviesList movies={this.props.relatedMovies} itemsPerRow={4} />
+            </div> : null
+          }
         </div>
       </div>
     )
